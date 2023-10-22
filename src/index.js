6db376af440364af9dd0fba84687bf804be99aeb
ASV185 <74805696+ASV185@users.noreply.github.com>
2023-10-22 14:53:35 -0600

    added if statement to cities in function update time

--- src/index.js
+++ src/index.js
@@ -1,47 +1,55 @@
 function updateTime() {
   //San Francisco
   let sanFranciscoElement = document.querySelector("#san-francisco");
-  let sanFranciscoDateElement = sanFranciscoElement.querySelector(".date");
-  let sanFranciscoTimeElement = sanFranciscoElement.querySelector(".time");
-  let sanFranciscoTime = moment().tz("US/Pacific");
+  if (sanFranciscoElement) {
+    let sanFranciscoDateElement = sanFranciscoElement.querySelector(".date");
+    let sanFranciscoTimeElement = sanFranciscoElement.querySelector(".time");
+    let sanFranciscoTime = moment().tz("US/Pacific");
 
-  sanFranciscoDateElement.innerHTML = sanFranciscoTime.format("MMMM D, YYYY");
-  sanFranciscoTimeElement.innerHTML = `${sanFranciscoTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  )}`;
+    sanFranciscoDateElement.innerHTML = sanFranciscoTime.format("MMMM D, YYYY");
+    sanFranciscoTimeElement.innerHTML = `${sanFranciscoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 
   //Madrid
   let madridElement = document.querySelector("#madrid");
-  let madridDateElement = madridElement.querySelector(".date");
-  let madridTimeElement = madridElement.querySelector(".time");
-  let madridTime = moment().tz("Europe/Madrid");
+  if (madridElement) {
+    let madridDateElement = madridElement.querySelector(".date");
+    let madridTimeElement = madridElement.querySelector(".time");
+    let madridTime = moment().tz("Europe/Madrid");
 
-  madridDateElement.innerHTML = madridTime.format("MMMM D, YYYY");
-  madridTimeElement.innerHTML = `${madridTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  )}`;
+    madridDateElement.innerHTML = madridTime.format("MMMM D, YYYY");
+    madridTimeElement.innerHTML = `${madridTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 
   //London
   let londonElement = document.querySelector("#london");
-  let londonDateElement = londonElement.querySelector(".date");
-  let londonTimeElement = londonElement.querySelector(".time");
-  let londonTime = moment().tz("Europe/London");
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector(".date");
+    let londonTimeElement = londonElement.querySelector(".time");
+    let londonTime = moment().tz("Europe/London");
 
-  londonDateElement.innerHTML = londonTime.format("MMMM D, YYYY");
-  londonTimeElement.innerHTML = `${londonTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  )}`;
+    londonDateElement.innerHTML = londonTime.format("MMMM D, YYYY");
+    londonTimeElement.innerHTML = `${londonTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 
   //New York
   let newYorkElement = document.querySelector("#new-york");
-  let newYorkDateElement = newYorkElement.querySelector(".date");
-  let newYorkTimeElement = newYorkElement.querySelector(".time");
-  let newYorkTime = moment().tz("Europe/Madrid");
+  if (newYorkElement) {
+    let newYorkDateElement = newYorkElement.querySelector(".date");
+    let newYorkTimeElement = newYorkElement.querySelector(".time");
+    let newYorkTime = moment().tz("Europe/Madrid");
 
-  newYorkDateElement.innerHTML = newYorkTime.format("MMMM D, YYYY");
-  newYorkTimeElement.innerHTML = `${newYorkTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  )}`;
+    newYorkDateElement.innerHTML = newYorkTime.format("MMMM D, YYYY");
+    newYorkTimeElement.innerHTML = `${newYorkTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 }
 
 function updateCity(event) {
